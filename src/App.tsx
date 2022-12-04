f19fa320f5a7228dfd9d357dbc0e18379c3fce9c
Pedro Papini <ppapinia@gmail.com>
2022-12-04 14:22:36 -0300

    Adiciona esqueleto create-react-app e arquivos iniciais dos componentes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Picture from './components/Picture/Picture';
+import Title from './components/Title/Title';
+
+function App() {
+  return (
+    <>
+      <Title />
+      <Picture />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <>
+      <h1>Astronomy Picture of the Day</h1>
+      <h2>Descrição</h2>
+    </>
+  )
+}
